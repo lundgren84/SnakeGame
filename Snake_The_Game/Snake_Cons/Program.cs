e1f080c0bd2a071e1c61e1558885f07505db7a83
lundgren84 <lundgren84@hotmail.se>
2016-04-07 16:53:42 +0200

    J

---
+++ Snake_The_Game/Snake_Cons/Program.cs
@@ -0,0 +1,206 @@
+﻿using Snake_Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake_Cons
+{
+    class Program
+    {
+        List<Circle> Snake;
+        Circle food;
+        bool eating = false;
+        Circle FoodEating;
+
+        public void PrintSnake()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(food.x, food.y);
+            Console.BackgroundColor = ConsoleColor.Red; Console.WriteLine(" ");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            if (Snake != null)
+                for (int i = 0; i < Snake.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        try
+                        {
+                            Console.SetCursorPosition(Snake[i].x, Snake[i].y);
+                            Console.BackgroundColor = ConsoleColor.DarkGreen; Console.WriteLine(" ");
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        catch
+                        {
+                            Console.Clear();
+                            Console.WriteLine("GameOver");
+                            CheckScore();
+
+                            Environment.Exit(-1);
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(Snake[i].x, Snake[i].y);
+                        Console.BackgroundColor = ConsoleColor.Green; Console.WriteLine(" ");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                }
+        }
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(30, 30);
+            Program p = new Program();
+            p.Game();
+        }
+        public void Game()
+        {
+            Thread dirChange = new Thread(ChangeDirection);
+            dirChange.Start();
+            StartGame();
+            while (true)
+            {
+                PrintSnake();
+                MoveSnake();
+                
+                SnakeEat();
+                Thread.Sleep(100);
+                Colosion();
+            }
+        }
+        public void StartGame()
+        {
+            new Settings();
+            Snake = new List<Circle>();
+            Circle Head = new Circle(10, 10);
+            Snake.Add(Head);
+            MakeFood();
+        }
+        private static void CheckScore()
+        {
+            Console.SetCursorPosition(1, 1);
+            Console.WriteLine("      Your Score is: " + Settings.score);
+            Console.ReadKey();
+        }
+        public void SnakeEat()
+        {
+            if (eating)
+            {
+                Snake.Add(FoodEating);
+                MakeFood();
+                eating = false;
+            }
+            if (Snake[0].x == food.x && Snake[0].y == food.y)
+            {
+                FoodEating = new Circle(food.x, food.y);
+                eating = true;
+                Settings.score += Settings.points;
+            }
+        }
+        public void MakeFood()
+        {
+            Random random = new Random();
+            int x = random.Next(1, 30);
+            int y = random.Next(1, 30);
+            food = new Circle(x, y);
+        }
+        public void MoveSnake()
+        {
+            for (int i = Snake.Count - 1; i >= 0; i--)
+            {
+                if (i > 0)
+                {
+                    Snake[i].x = Snake[i - 1].x;
+                    Snake[i].y = Snake[i - 1].y;
+                }
+                if (i == 0)
+                {
+                    try
+                    {
+                        if (Settings.direction == Direction.Down) { Snake[i].y++; }
+                        else if (Settings.direction == Direction.Up) { Snake[i].y--; }
+                        else if (Settings.direction == Direction.Left) { Snake[i].x--; }
+                        else if (Settings.direction == Direction.Right) { Snake[i].x++; }
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.WriteLine("GameOver");
+                        CheckScore();
+
+                        Environment.Exit(-1);
+                    }
+                }
+            }
+        }
+        public static void ChangeDirection()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo key;
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Spacebar:
+                        Settings.direction = Direction.Stop;
+                        CheckScore();
+                        break;
+                    case ConsoleKey.Enter:
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (Settings.direction != Direction.Right)
+                        {
+                            Settings.direction = Direction.Left;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (Settings.direction != Direction.Down)
+                        {
+                            Settings.direction = Direction.Up;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (Settings.direction != Direction.Left)
+                        {
+                            Settings.direction = Direction.Right;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (Settings.direction != Direction.Up)
+                        {
+                            Settings.direction = Direction.Down;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+        public void Colosion()
+        {
+            for (int i = 1; i < Snake.Count; i++)
+            {
+                if (Snake[0].x == Snake[i].x && Snake[0].y == Snake[i].y)
+                {
+                    Console.Clear();
+                    Console.WriteLine("GameOver");
+                    CheckScore();
+                    Environment.Exit(-1);
+                }
+            }
+            if(Snake[0].x == 30 || Snake[0].y == 30)
+            {
+                Console.Clear();
+                Console.WriteLine("GameOver");
+                CheckScore();
+                Environment.Exit(-1);
+            }
+        }
+
+    }
+}
---
+++ Snake_The_Game/Snake_Logic/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Logic
+{
+    public enum Direction
+    {
+        Up,Down,Left,Right,Stop
+    }
+   public class Settings
+    {
+       public static Direction direction { get; set; }
+        public static int wide { get; set; }
+        public int height { get; set; }
+        public static int points { get; set; }
+        public static int score { get; set; }
+       public  Settings()
+        {
+            direction = Direction.Stop;
+            points = 100;
+            score = 0;
+        }
+    }
+}
